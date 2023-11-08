ed48dc6cb6a8397de18e2f1563b1b61866ed467d
Mimir <mimirz@proton.me>
2023-11-08 21:43:31 +0100

    Fix for tests

--- src/env.rs
+++ src/env.rs
@@ -1,9 +1,11 @@
-use std::env::{VarError, var};
+use std::env::{var, VarError};
 
 use dotenv::dotenv;
 
 pub fn load_env() {
-    dotenv().expect("Error loading .env");
+    if let Err(error) = dotenv() {
+        error!("Error loading variables from .env: {}", error);
+    }
 }
 
 pub fn get(key: &str) -> Result<String, VarError> {
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+#[macro_use]
+extern crate log;
+
 pub mod env;
 pub mod web3;
 pub mod get_asset_transfers;
